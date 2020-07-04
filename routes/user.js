ea1efc2696ab6e2417e921dbce8b17adf8fe822b
vishnu0179 <vjvj140@gmail.com>
2020-07-05 02:50:06 +0530

    check Chat App --devcommit

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,15 @@
 const express = require('express')
 const router = express.Router()
+const checkU = require('../scripts/checkUser')
 
-router.get('/:id', (req, res)=>{
-    res.send(req.params.id);
+router.get('/:id', async (req, res)=>{
+    let userName  = req.params.id
+        
+    let result = await checkU(userName);
+    let data = await JSON.parse(result)
+    console.log(data)
+    res.send(data.data)
+
 })
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
 
+const SocketServer = require('websocket').server;
+
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
 const port = process.env.PORT  || 5500
 
 const user = require('./routes/user')
@@ -8,6 +13,28 @@ const user = require('./routes/user')
 app.use('/user' ,user);
 
 app.use(express.json());
+
+wsServer = new SocketServer({httpServer:app})
+
+
+wsServer.on('request',(req)=>{
+    const connection=req.accept()
+    console.log('new connection')
+    connections.push(connection)
+
+    connection.on('message',(mes)=>{
+        connections.forEach(element => {
+            if(element!=connection)
+                element.sendUTF(mes.utf8Data)
+        })
+    })
+
+    connection.on('close',(resCode,des)=>{
+        console.log('connection closed')
+        connections.splice(connections.indexOf(connection),1)
+    })
+})
+
 
 app.get('/ping', (req, res)=> {
     res.send('Success')
