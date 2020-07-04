580c18550bbb4df4636bbc87f857114d670a6c5b
vishnu0179 <vjvj140@gmail.com>
2020-07-05 02:57:53 +0530

    check Chat App --devcommit

--- server.js
+++ server.js
@@ -17,21 +17,23 @@ app.use(express.json());
 wsServer = new SocketServer({httpServer:app})
 
 
-wsServer.on('request',(req)=>{
-    const connection=req.accept()
-    console.log('new connection')
-    connections.push(connection)
-
-    connection.on('message',(mes)=>{
-        connections.forEach(element => {
-            if(element!=connection)
-                element.sendUTF(mes.utf8Data)
+app.get('/', (req, res)=> {
+    wsServer.on('request',(req)=>{
+        const connection=req.accept()
+        console.log('new connection')
+        connections.push(connection)
+    
+        connection.on('message',(mes)=>{
+            connections.forEach(element => {
+                if(element!=connection)
+                    element.sendUTF(mes.utf8Data)
+            })
         })
-    })
-
-    connection.on('close',(resCode,des)=>{
-        console.log('connection closed')
-        connections.splice(connections.indexOf(connection),1)
+    
+        connection.on('close',(resCode,des)=>{
+            console.log('connection closed')
+            connections.splice(connections.indexOf(connection),1)
+        })
     })
 })
 
