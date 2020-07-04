2c55e62dd4addf8d943fc4828534f26f00b9c49a
vishnu0179 <vjvj140@gmail.com>
2020-07-05 03:59:05 +0530

    dev chanes

--- server.js
+++ server.js
@@ -1,31 +1,43 @@
-const SocketServer = require('websocket').server
-const http = require('http')
+const express = require('express')
+const app = express()
 
-const port = process.env.PORT || 5500
 
-const server = http.createServer((req,res)=>{})
-server.listen(port,()=>{
-    console.log("Listening on port 5500...")
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+const port = process.env.PORT  || 5500
+
+const user = require('./routes/user')
+
+app.use('/user' ,user);
+
+app.use(express.json());
+
+
+io.on('connection', function(socket){
+
+    console.log('User Conncetion');
+  
+    socket.on('connect user', function(user){
+      console.log("Connected user ");
+      io.emit('connect user', user);
+    });
+  
+    socket.on('on typing', function(typing){
+      console.log("Typing.... ");
+      io.emit('on typing', typing);
+    });
+  
+    socket.on('chat message', function(msg){
+      console.log("Message " + msg['message']);
+      io.emit('chat message', msg);
+    });
+  });
+
+app.get('/ping', (req, res)=> {
+    res.send('Success')
 })
 
-wsServer = new SocketServer({httpServer:server})
-
-const connections=[]
-
-wsServer.on('request',(req)=>{
-    const connection=req.accept()
-    console.log('new connection')
-    connections.push(connection)
-
-    connection.on('message',(mes)=>{
-        connections.forEach(element => {
-            if(element!=connection)
-                element.sendUTF(mes.utf8Data)
-        })
-    })
-
-    connection.on('close',(resCode,des)=>{
-        console.log('connection closed')
-        connections.splice(connections.indexOf(connection),1)
-    })
+app.listen(port, ()=>{
+    console.log('app listening on', port);
 })
