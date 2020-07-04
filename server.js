47f5401138a59634720027101f9bb87289eeb1af
vishnu0179 <vjvj140@gmail.com>
2020-07-05 05:21:36 +0530

    dev chnges

--- server.js
+++ server.js
@@ -1,36 +1,42 @@
 const express = require('express')
-var app = express();
+const app = express()
+
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.set('port', (process.env.PORT || 5000));
+const port = process.env.PORT  || 5500
 
+const user = require('./routes/user')
 
-// views is directory for all template files
-;
+app.use('/user' ,user);
 
-console.log("outside io");
+app.use(express.json());
+
 
 io.on('connection', function(socket){
 
-  console.log('User Conncetion');
-
-  socket.on('connect user', function(user){
-    console.log("Connected user ");
-    io.emit('connect user', user);
+    console.log('User Conncetion');
+  
+    socket.on('connect user', function(user){
+      console.log("Connected user ");
+      io.emit('connect user', user);
+    });
+  
+    socket.on('on typing', function(typing){
+      console.log("Typing.... ");
+      io.emit('on typing', typing);
+    });
+  
+    socket.on('chat message', function(msg){
+      console.log("Message " + msg['message']);
+      io.emit('chat message', msg);
+    });
   });
 
-  socket.on('on typing', function(typing){
-    console.log("Typing.... ");
-    io.emit('on typing', typing);
-  });
+app.get('/ping', (req, res)=> {
+    res.send('Success')
+})
 
-  socket.on('chat message', function(msg){
-    console.log("Message " + msg['message']);
-    io.emit('chat message', msg);
-  });
-});
-
-http.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+app.listen(port, ()=>{
+    console.log('app listening on', port);
+})
