87e61a208b3d829c4c662125ffe97995dda83b61
vishnu0179 <vjvj140@gmail.com>
2020-07-11 10:46:43 +0530

    created a pool connection

--- models/db.js
+++ models/db.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql')
 const dbConfig = require('../config/db.config.js')
 
-const mysqlConnection = mysql.createConnection({
+const mysqlConnection = mysql.createPool({
     host: dbConfig.HOST,
     user: dbConfig.USER,
     password: dbConfig.PASSWORD,
@@ -9,12 +9,12 @@ const mysqlConnection = mysql.createConnection({
     multipleStatements: true
 });
 
-mysqlConnection.connect((err) => {
-    if (!err)
-        console.log('DB connection succesfull');
-    else
-        console.log('DB connection failed\n Error : ' + JSON.stringify(err, undefined, 2));
-});
+// mysqlConnection.connect((err) => {
+//     if (!err)
+//         console.log('DB connection succesfull');
+//     else
+//         console.log('DB connection failed\n Error : ' + JSON.stringify(err, undefined, 2));
+// });
 
 
 module.exports = mysqlConnection;
